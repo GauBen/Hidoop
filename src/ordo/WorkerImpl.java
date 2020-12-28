33a4570bee14bd13a2d8ba1a3b98257bf189beaa
Guillaume Claverie <guillaume.claverie@mail.com>
2020-12-28 16:57:25 +0100

    Gestions de + d'erreurs

--- src/ordo/WorkerImpl.java
+++ src/ordo/WorkerImpl.java
@@ -7,6 +7,7 @@ import map.Mapper;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
@@ -26,12 +27,14 @@ public class WorkerImpl extends UnicastRemoteObject implements Worker {
 
     protected WorkerImpl() throws RemoteException {
         super();
+
     }
 
     static int port = 4000;
 
     public static void usage() {
         System.out.println("Usage : WorkerImpl <id>");
+        System.out.println("id >= 0");
     }
 
     public static void main(String[] args) {
@@ -41,11 +44,54 @@ public class WorkerImpl extends UnicastRemoteObject implements Worker {
                 return;
             }
             WorkerImpl worker = new WorkerImpl();
+
+            try {
+                Integer.parseInt(args[0]);
+                if (Integer.parseInt(args[0]) < 0) {
+                    usage();
+                    return;
+                }
+            } catch (NumberFormatException e) {
+                usage();
+                return;
+            }
+
             worker.id = args[0]; // Id has to be a number between 0 and the total number of nodes minus 1
+
+
+            try {
+
+
+                Naming.lookup(WorkerImpl.serverAddress + ":" + WorkerImpl.port + "/Node" + worker.id);
+                System.out.println("Attention, cet ID a déjà été enregistré dans le RMIRegistry. Est-ce un doublon ou un redemarrage ?");
+
+            } catch (NotBoundException e) {
+                // OK
+
+            } catch (MalformedURLException | RemoteException e) {
+                System.out.println("Le rmi registry n'est pas disponible sur " + Job.serverAddress + ":" + Job.port);
+                System.exit(1);
+            }
 
             Naming.rebind(WorkerImpl.serverAddress + ":" + WorkerImpl.port + "/Node" + worker.id, worker);
             System.out.println("Worker" + args[0] + " bound in registry");
 
+            /* Tentative de unbind lorsque le node s'arrête
+            PrintStream stream = System.out;
+
+            Runtime.getRuntime().addShutdownHook(new Thread(){
+                @Override
+                public void run() {
+                    super.run();
+                    try {
+                        stream.println("Let's unbind!");
+                        Naming.unbind(WorkerImpl.serverAddress + ":" + WorkerImpl.port + "/Node" + worker.id);
+                    } catch (RemoteException | NotBoundException | MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        */
         } catch (RemoteException e) {
             System.out.println("Il semble que le rmiregistry ne contienne pas les classes nécessaires! Etes vous sur de l'avoir executé dans le bon dossier ?");
         } catch (MalformedURLException e) {
