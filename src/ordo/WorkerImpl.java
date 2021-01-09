2cf628dd65a29af93963930fe7496269b33b07e4
Guillaume Claverie <guillaume.claverie@mail.com>
2021-01-09 15:53:49 +0100

    Deplacement de la création dans le constructeur au lieu du main

--- src/ordo/WorkerImpl.java
+++ src/ordo/WorkerImpl.java
@@ -25,78 +25,73 @@ public class WorkerImpl extends UnicastRemoteObject implements Worker {
      */
     static String serverAddress = "//localhost";
 
-    protected WorkerImpl() throws RemoteException {
+    static int port = 1; // Offset du port
+
+    public WorkerImpl(String hostDuRmi, int portDuRmi, String hostDistantDuNoeudHdfs, int portDuNodeHdfs) throws RemoteException {
         super();
 
-    }
-
-    static int port = 4000;
-
-    public static void usage() {
-        System.out.println("Usage : WorkerImpl <id>");
-        System.out.println("id >= 0");
-    }
-
-    public static void main(String[] args) {
         try {
-            if (args.length < 1) {
-                usage();
-                return;
-            }
-            WorkerImpl worker = new WorkerImpl();
+            Naming.lookup(hostDuRmi + ":" + portDuRmi + "/Node(" + hostDistantDuNoeudHdfs + " : " + portDuNodeHdfs + ")"); //TODO: remplacer la convention par un appel de fonction
+            System.out.println("Attention, cet ID a déjà été enregistré dans le RMIRegistry. Est-ce un doublon ou un redemarrage ?");
 
-            try {
-                Integer.parseInt(args[0]);
-                if (Integer.parseInt(args[0]) < 0) {
-                    usage();
-                    return;
-                }
-            } catch (NumberFormatException e) {
-                usage();
-                return;
-            }
+        } catch (NotBoundException e) {
+            // OK
 
-            worker.id = args[0]; // Id has to be a number between 0 and the total number of nodes minus 1
+        } catch (MalformedURLException | RemoteException e) {
+            System.out.println("Le rmi registry n'est pas disponible sur " + Job.serverAddress + ":" + Job.port);
+            System.exit(1);
+        }
 
+        try {
+            Naming.rebind(hostDuRmi + ":" + portDuRmi + "/Node(" + hostDistantDuNoeudHdfs + " : " + portDuNodeHdfs + ")", this);
+            System.out.println("Worker" + "/Node(" + hostDistantDuNoeudHdfs + " : " + portDuNodeHdfs + ")" + " bound in registry");
 
-            try {
-
-
-                Naming.lookup(WorkerImpl.serverAddress + ":" + WorkerImpl.port + "/Node" + worker.id);
-                System.out.println("Attention, cet ID a déjà été enregistré dans le RMIRegistry. Est-ce un doublon ou un redemarrage ?");
-
-            } catch (NotBoundException e) {
-                // OK
-
-            } catch (MalformedURLException | RemoteException e) {
-                System.out.println("Le rmi registry n'est pas disponible sur " + Job.serverAddress + ":" + Job.port);
-                System.exit(1);
-            }
-
-            Naming.rebind(WorkerImpl.serverAddress + ":" + WorkerImpl.port + "/Node" + worker.id, worker);
-            System.out.println("Worker" + args[0] + " bound in registry");
-
-            /* Tentative de unbind lorsque le node s'arrête
-            PrintStream stream = System.out;
-
-            Runtime.getRuntime().addShutdownHook(new Thread(){
-                @Override
-                public void run() {
-                    super.run();
-                    try {
-                        stream.println("Let's unbind!");
-                        Naming.unbind(WorkerImpl.serverAddress + ":" + WorkerImpl.port + "/Node" + worker.id);
-                    } catch (RemoteException | NotBoundException | MalformedURLException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-        */
         } catch (RemoteException e) {
             System.out.println("Il semble que le rmiregistry ne contienne pas les classes nécessaires! Etes vous sur de l'avoir executé dans le bon dossier ?");
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
+
+    }
+
+
+
+    public static void usage() {
+        System.out.println("Usage : WorkerImpl <hostDuRmi> <portDuRmi> <hostDistantDuNoeudHdfs> <portDuNodeHdfs>");
+        System.out.println("id >= 0");
+    }
+
+    public static void main(String[] args) {
+
+        if (args.length < 4) {
+            usage();
+            return;
+        }
+
+        try {
+            Integer.parseInt(args[1]);
+            Integer.parseInt(args[3]);
+            if (Integer.parseInt(args[0]) < 0) {
+                usage();
+                return;
+            }
+        } catch (NumberFormatException e) {
+            usage();
+            return;
+        }
+
+        String hostDuRmi = args[0];
+        int portDuRmi = Integer.parseInt(args[1]);
+        String hostDuNoeudHdfs = args[2];
+        int portDuNoeudHdfs = Integer.parseInt(args[3]);
+
+        WorkerImpl worker;
+        try {
+            worker = new WorkerImpl(hostDuRmi, portDuRmi, hostDuNoeudHdfs, portDuNoeudHdfs);
+        } catch (RemoteException e1) {
+            e1.printStackTrace();
+        }
+
     }
 
     @Override
