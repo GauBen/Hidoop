078a5c100fd5f9eb926667ef43a2fcef3e043864
Guillaume Claverie <guillaume.claverie@mail.com>
2021-02-21 22:07:33 +0100

    re re re re fix exec

--- exec.sh
+++ exec.sh
@@ -2,20 +2,18 @@
 USERNAME=$USER
 HOSTS="pikachu carapuce salameche"
 
-CURRENT_HOST=`echo $HOSTNAME` # Le nom du serveur qui héberge le HdfsServer et le rmiserver ATTENTION a verifier si c'est defini sur les pc de l'enseeiht
-NameserverPort=30000
+CURRENT_HOST=$HOSTNAME # Le nom du serveur qui héberge le HdfsServer et le rmiserver ATTENTION a verifier si c'est defini sur les pc de l'enseeiht
+NameserverPort=51200
 RmiserverPort=4000
 
 CUSTOM_PATH="/work/"$USERNAME"/Hidoop"
 
 
 # Le script execute sur chaque machine - adresse du nameserver, port du nameserver, nom du dossier dans lequel y'a les fichiers de Hdfs,adresse du RMI, port du RMI
-SCRIPT="java -cp $CUSTOM_PATH application.BiNode ${CURRENT_HOST} ${NameserverPort} node/ ${CURRENT_HOST} ${RmiserverPort}  > BiNodeLog.log"
+SCRIPT="java -cp $CUSTOM_PATH application.BiNode ${CURRENT_HOST} ${NameserverPort} ${CUSTOM_PATH}/node/ ${CURRENT_HOST} ${RmiserverPort}  > BiNodeLog.log"
 
 # On demarre le RMI
 java application.RmiCustom ${RmiserverPort} & # On fait un RMI registry en tache de fond
-
-java "hdfs.HdfsNameServer" $NameserverPort
 
 # Execution des commandes sur les machines distantes
 for HOST in ${HOSTS} ; do
@@ -23,18 +21,19 @@ for HOST in ${HOSTS} ; do
     ssh -l ${USERNAME} ${HOST} "${SCRIPT}" &
 done
 
-java "hdfs.HdfsNameServer" & # TODO : ajouter les argumen`ts
+java "hdfs.HdfsNameServer" $NameserverPort & # TODO : ajouter les argumen`ts
 
-# TODO : STOPPER LES SERVEURS LORSQUE ON APPUIE SUR UN CARACTERE !
-echo "Appuyez sur un bouton pour arreter"
+# STOPPER LES SERVEURS LORSQUE ON APPUIE SUR UN CARACTERE !
+
 while [ true ] ; do
+echo "Appuyez sur un bouton pour arreter"
 read -t 3 -n 1
 if [ $? = 0 ] ; then
 exit ;
 fi
 done
 
-pkill -9 -f RmiCustom
+pkill -9 -f RmiCustom &
 
 echo "On stoppe les serveurs..."
 for HOST in ${HOSTS} ; do
