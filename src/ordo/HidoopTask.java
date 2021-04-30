7fe748a3e7b44d149e867c53a178c41f403f9eaf
Guillaume Claverie <guillaume.claverie@mail.com>
2021-04-30 15:13:23 +0200

    List entiers au lieu de juste entier

--- src/ordo/HidoopTask.java
+++ src/ordo/HidoopTask.java
@@ -1,12 +1,14 @@
 package ordo;
 
+import java.util.List;
+
 public class HidoopTask {
     public String nom;
 
-    public int dataEntier;
+    public List<Integer> dataEntier;
     public String dataString;
 
-    public HidoopTask(String nom, int dataEntier, String dataString) {
+    public HidoopTask(String nom, List<Integer> dataEntier, String dataString) {
         this.nom = nom;
         this.dataEntier = dataEntier;
         this.dataString = dataString;
