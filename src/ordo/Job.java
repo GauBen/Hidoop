0e2525cc70aed81e783d68697effddfed959030a
Guillaume Claverie <guillaume.claverie@mail.com>
2021-04-30 21:48:38 +0200

    Fragmentname au lieu de filename

--- src/ordo/Job.java
+++ src/ordo/Job.java
@@ -287,7 +287,7 @@ public class Job implements JobInterfaceX {
         FragmentInfo fragmentDuResultat = new FragmentInfo(getTempFileName(), idTask, idTask == (tasks.size() - 1), workerUri,
                 workerUri.getRoot());
 
-        Format oFormat = this.getFormatFromType(this.outputFormat, fragmentDuResultat.filename);
+        Format oFormat = this.getFormatFromType(this.outputFormat, fragmentDuResultat.getFragmentName());
 
         try {
             worker.runFileLessMap((FileLessMapperReducer) this.mapReduce, task, oFormat, callBack);
